744bde2c300f3f4ebf89ad48e5f371b7c5a947b3
Hemenhk <hemen.hiwakamal@hotmail.com>
2024-07-16 13:46:39 +0200

    filter out booked times

--- src/models/appointment.ts
+++ src/models/appointment.ts
@@ -2,21 +2,63 @@ import { Schema, model, Document, Types } from "mongoose";
 
 interface IAppointment extends Document {
   name: string;
-  user: Types.ObjectId;
+  last_name: string;
+  email: string;
+  phone_number: string;
   service: Types.ObjectId;
-  date: Date;
+  date: string;
   time: string;
   status: string;
 }
 
-const appointmentSchema = new Schema<IAppointment>({
-  name: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: false },
-  service: { type: Schema.Types.ObjectId, ref: "Service", required: true },
-  date: { type: Date, required: false },
-  time: { type: String, required: true },
-  status: { type: String, default: "Pending" },
-});
+const appointmentSchema = new Schema<IAppointment>(
+  {
+    name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    service: { type: Schema.Types.ObjectId, ref: "Service", required: true },
+    date: {
+      type: String,
+      required: false,
+      default: () => {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+        const day = String(currentDate.getDate()).padStart(2, "0");
+        return `${year}-${month}-${day}`;
+      },
+    },
+    time: { type: String, required: true },
+    status: { type: String, default: "Pending" },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        if (ret.date) {
+          const dateObj = new Date(ret.date);
+          const day = String(dateObj.getDate()).padStart(2, "0");
+          const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+          const year = dateObj.getFullYear();
+          ret.date = `${day}/${month}/${year}`;
+        }
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        if (ret.date) {
+          const dateObj = new Date(ret.date);
+          const day = String(dateObj.getDate()).padStart(2, "0");
+          const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+          const year = dateObj.getFullYear();
+          ret.date = `${day}/${month}/${year}`;
+        }
+      },
+    },
+  }
+);
 
 export const Appointment = model<IAppointment>(
   "Appointment",
--- src/routes/appointments.ts
+++ src/routes/appointments.ts
@@ -9,23 +9,33 @@ const router = Router();
 router.get("/available-times", async (req, res) => {
   try {
     const date = req.query.date as string;
+    console.log("date", date);
     if (!date) {
       return res.status(400).send({ error: "Date is required" });
     }
+
+    const formattedDate = date.split("/").reverse().join("-");
 
     const startTime = "09:00";
     const endTime = "18:00";
     const timeSlots = generateTimeSlots(startTime, endTime);
 
-    const appointments = await Appointment.find({ date: new Date(date) });
+    // Find appointments for the given date
+    const appointments = await Appointment.find({ date: formattedDate });
+    console.log("appoint", appointments);
+
+    // Extract booked times from appointments
     const bookedTimes = appointments.map((app) => app.time);
 
+    // Filter out booked times from timeSlots
     const availableTimes = timeSlots.filter(
       (slot) => !bookedTimes.includes(slot)
     );
-
+    console.log("bookedtimes", bookedTimes);
+    console.log("availableTimes", availableTimes);
     res.status(200).send({ availableTimes });
   } catch (error: any) {
+    console.error("Error fetching available times:", error);
     res.status(500).send({ error: error.message });
   }
 });
@@ -33,15 +43,16 @@ router.get("/available-times", async (req, res) => {
 // Create an appointment
 router.post("/", async (req, res) => {
   try {
-    const { name, user, service, date, time } = req.body;
+    const { date, time } = req.body;
 
     const existingAppointment = await Appointment.findOne({ date, time });
     if (existingAppointment) {
       return res.status(400).send({ error: "Time slot is already booked" });
     }
 
-    const appointment = new Appointment({ name, user, service, date, time });
+    const appointment = new Appointment(req.body);
     await appointment.save();
+
     res.status(201).send(appointment);
   } catch (error: any) {
     res.status(400).send({ error: error.message });
